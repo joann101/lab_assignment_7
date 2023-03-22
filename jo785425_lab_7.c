eb47d74b9d5f94a370ae199b6699e6e33c3d4e7f
joann101 <joanndinzey@gmail.com>
2023-03-22 18:05:47 -0400

    lab assignment 7 updated(2)

--- jo785425_lab_7.c
+++ jo785425_lab_7.c
@@ -1,4 +1,3 @@
-#include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h> 
@@ -22,18 +21,9 @@ void bubbleSort(int array[], int n)
     }
 }
 
-void printArray(int array[], int len)
-{
-	for (int i = 0;i<len;++i)
-	{
-		printf("%d ",array[i]);
-	}
-}
-
 int main(void)
 {
     swaps=0;
     int array[9]={97, 16, 45, 63, 13, 22, 7, 58, 72};
     bubbleSort(array,9);
-    //printArray(array,9);
 }
